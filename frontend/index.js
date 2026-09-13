4067265159fb3199c3fb7c2f66c7e90b2b72bfd8
Shivam-38 <rt1882422@gmail.com>
2026-09-13 21:08:02 +0530

    Adding botton feature

--- frontend/index.js
+++ frontend/index.js
@@ -19,7 +19,7 @@ themebtn.addEventListener("click", function(){
 });
 
 //ADDING IMAGE WORK
-constinsertimg = document.getElementById("insertimg");
+const insertimg = document.getElementById("insertimg");
 const imgcanvas = document.getElementById("imgcanvas");
 const cxt = imgcanvas.getContext("2d");
 const placeholder = document.getElementById("placeholder");
@@ -37,7 +37,105 @@ insertimg.addEventListener("change", function(){
         cxt.drawImage(image, 0, 0);
 
         placeholder.style.display = "none";
-    }--+\;
+    };
 
     image.src = URL.createObjectURL(file);
+});
+
+// GRAYSCALE BUTTON
+const grayscaleBtn = document.getElementById("grayscaleBtn");
+
+grayscaleBtn.addEventListener("click", async function () {
+
+    imgcanvas.toBlob(async function(blob) {
+
+        const formData = new FormData();
+
+        formData.append("image", blob, "image.png");
+
+        const response = await fetch(
+            "http://127.0.0.1:5000/grayscale",
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        const resultBlob = await response.blob();
+
+        const imageURL = URL.createObjectURL(resultBlob);
+
+        const grayImage = new Image();
+
+        grayImage.onload = function() {
+
+            cxt.clearRect(
+                0,
+                0,
+                imgcanvas.width,
+                imgcanvas.height
+            );
+
+            cxt.drawImage(grayImage, 0, 0);
+
+            URL.revokeObjectURL(imageURL);
+        };
+
+        grayImage.src = imageURL;
+
+    }, "image/png");
+
+});
+
+//BLUR BOTTON
+const blurBtn = document.getElementById("blurBtn");
+
+blurBtn.addEventListener("click", async function () {
+
+    console.log("Blur button clicked");
+
+    imgcanvas.toBlob(async function(blob) {
+
+        const formData = new FormData();
+
+        formData.append("image", blob, "image.png");
+
+        const response = await fetch(
+            "http://127.0.0.1:5000/blur",
+            {
+                method: "POST",
+                body: formData
+            }
+        );
+
+        const resultBlob = await response.blob();
+
+        const imageURL = URL.createObjectURL(resultBlob);
+
+        const blurredImage = new Image();
+
+        blurredImage.onload = function() {
+
+            cxt.clearRect(
+                0,
+                0,
+                imgcanvas.width,
+                imgcanvas.height
+            );
+
+            cxt.drawImage(
+                blurredImage,
+                0,
+                0,
+                imgcanvas.width,
+                imgcanvas.height
+            );
+
+            URL.revokeObjectURL(imageURL);
+        };
+
+        blurredImage.src = imageURL;
+
+    }, "image/png");
+
 });
